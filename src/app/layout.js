34762f51ca68d9ecda1e70cf85d7c8d887ab138c
umayer10-AI <mdumayer10@gmail.com>
2026-04-29 08:29:10 +0600

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./Navbar";
+import Navbar from "../component/Navbar";
+import Footer from "@/component/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,8 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar></Navbar>
-        <main>{children}</main>
+        <main className="w-[90%] mx-auto">{children}</main>
+        <Footer></Footer>
         </body>
     </html>
   );
