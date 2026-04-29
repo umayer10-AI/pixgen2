eb88e319d3580e986ed99817d5a145ad577f5db2
umayer10-AI <mdumayer10@gmail.com>
2026-04-29 13:05:34 +0600

    add

--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -1,17 +1,17 @@
 import { getCategory } from '@/lib/fetching';
 import { Button } from '@heroui/react';
+import Link from 'next/link';
 import React from 'react';
 
 const Category = async () => {
 
     const data = await getCategory()
-    // console.log(data)
 
     return (
         <div className='flex items-center justify-center gap-2 mb-5'>
             {
                 data.map(v => (
-                    <Button key={v.id} variant='outline'>{v.name}</Button>
+                    <Link href={`?category=${v.name}`} key={v.id}><Button variant='outline'>{v.name}</Button></Link>
                 ))
             }
         </div>
