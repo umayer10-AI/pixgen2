9ed6d3c61ad3a4a185839e00dbe96c509bb6de46
umayer10-AI <mdumayer10@gmail.com>
2026-04-29 09:58:04 +0600

    add

--- src/component/CartList.jsx
+++ src/component/CartList.jsx
@@ -1,5 +1,6 @@
-import { Button, Separator } from '@heroui/react';
+import { Button, Chip, Separator } from '@heroui/react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { FaHeart } from 'react-icons/fa';
 import { FiDownload } from 'react-icons/fi';
@@ -12,14 +13,15 @@ const CartList = ({p}) => {
                 src={p.imageUrl} 
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 alt='photo' className='object-cover rounded-xl'></Image>
+                <Chip size='sm' className='absolute top-2 right-2 px-1'>{p.category}</Chip>
             </div>
             <h2 className='font-bold'>{p.title}</h2>
             <div className='font-semibold flex gap-5'>
                 <h2 className='flex items-center gap-2'><FaHeart />{p.likes}</h2>
                 <Separator  orientation="vertical"></Separator>
-                <h2 className='flex items-center gap-2'><FiDownload className='text-xl' />{p.downloads}</h2>
+                <h2 className='flex items-center gap-1'><FiDownload className='text-xl' />{p.downloads}</h2>
             </div>
-            <Button className={'w-full bg-linear-to-r from-green-500 via-blue-500 bg-green-500'}>View</Button>
+            <Link href={`/all-photos/${p.id}`}><Button className={'w-full bg-linear-to-r from-green-500 via-blue-500 bg-green-500'}>View</Button></Link>
         </div>
     );
 };
