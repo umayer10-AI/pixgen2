5d06d667edd10107a11aa8f993ad12dabeffdbc1
umayer10-AI <mdumayer10@gmail.com>
2026-04-29 10:23:53 +0600

    add

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,92 @@
+"use client"
+import { Check } from '@gravity-ui/icons';
+import { Button, Card, Description, FieldError, Form, Input, Label, TextField } from '@heroui/react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+const SignUpPage = () => {
+
+    const {register,handleSubmit,formState: { errors }} = useForm()
+
+    const a = (v) => {
+        console.log(v)
+    }
+
+    return (
+        <div>
+            <Card className="border mx-auto w-90 lg:w-125 py-10 mt-5">
+      <h1 className="text-center text-2xl font-bold">Sign Up</h1>
+
+      <Form onSubmit={handleSubmit(a)} className="flex w-80 lg:w-96 mx-auto flex-col gap-4">
+        <TextField isRequired name="name" type="text">
+          <Label>Name</Label>
+          <Input placeholder="Enter your name"  {...register("name")}/>
+          <FieldError />
+        </TextField>
+
+        <TextField isRequired name="image" type="text">
+          <Label>Image URL</Label>
+          <Input placeholder="Image URL"  {...register("url")}/>
+          <FieldError />
+        </TextField>
+
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input placeholder="Enter your email"  {...register("email")}/>
+          <FieldError />
+        </TextField>
+
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input placeholder="Enter your password"  {...register("password")}/>
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </Card>
+        </div>
+    );
+};
+
+export default SignUpPage;
